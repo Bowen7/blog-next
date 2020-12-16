cab6bed34e830063253687e19ea603222bc5faa0
张文翔 <771797746@qq.com>
2020-12-16 22:02:36 +0800

    feat: add ga

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 // https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -27,5 +27,32 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-6KLLQLJ9LT"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-6KLLQLJ9LT');`
+            }}
+          />
+          <script></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
